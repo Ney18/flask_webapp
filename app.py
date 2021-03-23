4ac3c820c3340a202c7993e23b90195fc1aed80f
Ney18 <bernardlogmo@hotmail.fr>
2021-03-23 14:15:35 +0100

    First Commit flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/home")
+def hello():
+    return "Hello world"
+@app.route('/resultats')
+def submit():
+    return 'Success'
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True)
